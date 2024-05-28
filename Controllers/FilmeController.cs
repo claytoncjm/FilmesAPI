ee97136f6bb15c7dbba5e25fae4707bb2be757eb
Clayton <clayton.cjm@terra.com.br>
2024-05-28 18:36:20 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.DTOs;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        //criado context para acessar e guardar informações no banco
+        private FilmeContext _context;
+        private IMapper _mapper;
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]//inserir dados no sistema
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            //criando um objeto para adicionar ao sistema
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecupearFilmePorId), new {id = filme.Id }, filme);
+        }
+
+        [HttpGet]//recuperar ou exibir dados do sistema
+       public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return _context.Filmes;
+        }
+
+        [HttpGet("{id}")]//ultilizando parametros diferentes
+        public IActionResult RecupearFilmePorId(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme !=null)
+            {
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+        [HttpPut("{id}")]//buscando por Id para a edicao
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {//checa se o filme existe
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+           
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) 
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Data/DTOs/UpdateFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.DTOs
+{
+    public class UpdateFilmeDto
+    {
+       
+        [Required(ErrorMessage = "O Campo Titulo nao pode ficar Vazio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Campo Diretor nao pode ficar Vazio")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
+
